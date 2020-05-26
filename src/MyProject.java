4f990958d39ff1bda2205f5e480f5fda9333c121
Harry Carpenter <harrycarpenter@ozoneteam.net>
2020-05-26 12:50:39 +0800

    JDoc comments and removal of unnecessary functions

--- src/MyProject.java
+++ src/MyProject.java
@@ -15,60 +15,75 @@ public class MyProject {
         // Empty initializer
     }
 
-     public int floodFillCount(int[][] image, int row, int col){
+    /**
+     * Compute the number of pixels that change when performing a black flood-fill
+     * from the pixel at ( row , col ) in the given image. A flood-fill operation changes
+     * the selected pixel and all contiguous pixels of the same colour to the specified colour.
+     * A pixel is considered part of a contiguous region of the same colour if it is exactly
+     * one pixel up/down/left/right of another pixel in the region.
+     *
+     * @param image
+     * @param row
+     * @param col
+     * @return number of cells filled
+     */
+    public int floodFillCount(int[][] image, int row, int col) {
         //black flood fill
         int ff_init = image[row][col];
         //check to see if the initial is the colour wanting to be filled
-         // in this case, it's black
-         if(ff_init != 0){
-             int x = 2, y = 0, newC = 0;
-             return floodFill(image, row, col, newC);
-         }
+        // in this case, it's black
+        if (ff_init != 0) {
+//            return floodFill(image, row, col, 0);
+            return floodFillUtil(image, row, col, image[row][col], 0);
+        }
 
-         return ff_init;
+        return ff_init;
     }
 
-    static int floodFillUtil(int screen[][], int x, int y,
-                             int prevC, int newC)
-    {
+    static int floodFillUtil(int image[][], int x, int y, int prevC, int newC) {
         int FFCount = 0;
         // Base cases
-        if (x < 0 || x >= screen.length || y < 0 || y >= screen[0].length)
+        if (x < 0 || x >= image.length || y < 0 || y >= image[0].length)
             return 0;
-        if (screen[x][y] != prevC)
+        if (image[x][y] != prevC)
             return 0;
 
         // Replace the color at (x, y)
-        screen[x][y] = newC;
+        image[x][y] = newC;
         FFCount++;
         // Recur for north, east, south and west
-        FFCount += floodFillUtil(screen, x+1, y, prevC, newC);
-        FFCount += floodFillUtil(screen, x-1, y, prevC, newC);
-        FFCount += floodFillUtil(screen, x, y+1, prevC, newC);
-        FFCount += floodFillUtil(screen, x, y-1, prevC, newC);
+        FFCount += floodFillUtil(image, x + 1, y, prevC, newC);
+        FFCount += floodFillUtil(image, x - 1, y, prevC, newC);
+        FFCount += floodFillUtil(image, x, y + 1, prevC, newC);
+        FFCount += floodFillUtil(image, x, y - 1, prevC, newC);
         return FFCount;
     }
 
-    // It mainly finds the previous color on (x, y) and
-// calls floodFillUtil()
-    static int floodFill(int screen[][], int x, int y, int newC)
-    {
-        int prevC = screen[x][y];
-        return floodFillUtil(screen, x, y, prevC, newC);
-
-    }
-
-    public int brightestSquare(int[][] image, int k){
+    /**
+     * Compute the total brightness of the brightest exactly k*k square that appears in the given image.
+     * The total brightness of a square is defined as the sum of its pixel values.
+     * You may assume that k is positive, no greater than R or C, and no greater than 2048.
+     * @param image grayscale image to find the brightest square
+     * @param k the search area (k*k array)
+     * @return colour of brightest square
+     */
+ public int brightestSquare(int[][] image, int k) {
         if (k <= 0)
-            throw new ArrayIndexOutOfBoundsException("Chunks must be atleast 1x1");
+            throw new ArrayIndexOutOfBoundsException("Chunks must be at least 1x1");
+        // Get dimensions of the image
+        int length = image.length; //this is the rows
+        int height = image[0].length; //this is the columns
 
-        int length = image.length;
-        int height = image[0].length;
-
+        // Initialise variable to hold the highest value found in the
+        // square
         int highest = 0;
 
-        for(int xpos = 0; xpos < length; xpos++){
-            for(int ypos = 0; ypos < height; ypos ++){
+        // We now iterate through the square searching
+        // the area defined by K.
+        // it wil only check the squares it can fit in,
+        // thus increasing efficiency.
+        for (int xpos = 0; xpos < length; xpos++) {
+            for (int ypos = 0; ypos < height; ypos++) {
                 if (k + xpos <= length && k + ypos <= height) {
                     int summ = 0;
                     for (int x = xpos; x < k + xpos; x++) {
@@ -76,63 +91,85 @@ public class MyProject {
                             summ += image[x][y];
                         }
                     }
-                    if(summ > highest) highest = summ;
+                    if (summ > highest) highest = summ;
                 }
             }
         }
+        //return the highest value sum from the squares.
         return highest;
     }
 
+    /**
+     * Compute the maximum brightness that MUST be encountered when drawing a path from the pixel at (ur, uc) to the pixel at (vr, vc).
+     * The path must start at (ur, uc) and end at (vr, vc), and may only move one pixel up/down/left/right at a time in between.
+     * The brightness of a path is considered to be the value of the brightest pixel that the path ever touches.
+     * This includes the start and end pixels of the path.
+     *
+     * To do this, we had to construct a basic path finding algorithm between two points
+     * and compute it from there.
+     * @param image
+     * @param ur
+     * @param uc
+     * @param vr
+     * @param vc
+     * @return The minimum brightness of any path from (ur, uc) to (vr, vc)
+     */
+    public int darkestPath(int[][] image, int ur, int uc, int vr, int vc) {
 
-    public int darkestPath(int[][] image, int ur, int uc, int vr, int vc){
-
-        int row = ur; int col = uc;
-        int dr = vr; int dc = vc;
+        int row = ur;
+        int col = uc;
+        int dr = vr;
+        int dc = vc;
         int rowDiff = dr - row;
         int colDiff = dc - col;
 
         int[][] subArray = new int[Math.abs(rowDiff) + 1][Math.abs(colDiff) + 1];
         int rx = 0;
 
-        if(rowDiff >= 0 && colDiff >= 0){
+
+        // The following check to see the direction of the path (N, S, E, W, NW, NE, SW, SE)
+        // and flip it appropriately to fit into a top-left bottom-right array for the path
+        // finding algorithm, and creates a child array which holds the values only between the
+        // two points.
+        if (rowDiff >= 0 && colDiff >= 0) {
             //rd +ve
             for (int i = row; i <= dr; i++) {
                 int cx = 0;
                 //cd+ve
-                for(int c = col; c <= dc; c++ ){
+                for (int c = col; c <= dc; c++) {
                     subArray[rx][cx] = image[i][c];
                     cx++;
                 }
                 rx++;
             }
-        } else if(rowDiff >= 0 && colDiff < 0) {
+        } else if (rowDiff >= 0 && colDiff < 0) {
             //rd +ve
             for (int i = row; i <= dr; i++) {
                 int cx = 0;
                 //cd -ve
-                for(int c = col; c >= dc; c-- ){
+                for (int c = col; c >= dc; c--) {
                     subArray[rx][cx] = image[i][c];
                     cx++;
                 }
                 rx++;
             }
-        } else if (rowDiff < 0 && colDiff < 0){
+        } else if (rowDiff < 0 && colDiff < 0) {
             //rd -ve
             for (int i = row; i >= dr; i--) {
                 int cx = 0;
                 //cd -ve
-                for(int c = col; c >= dc; c-- ){
+                for (int c = col; c >= dc; c--) {
                     subArray[rx][cx] = image[i][c];
                     cx++;
                 }
                 rx++;
             }
-        } else if (rowDiff < 0 && colDiff >= 0){
+        } else if (rowDiff < 0 && colDiff >= 0) {
             //rd -ve
             for (int i = row; i >= dr; i--) {
                 int cx = 0;
                 //cd -ve
-                for(int c = col; c <= dc; c++ ){
+                for (int c = col; c <= dc; c++) {
                     subArray[rx][cx] = image[i][c];
                     cx++;
                 }
@@ -143,8 +180,7 @@ public class MyProject {
         int m = subArray.length; //height;
         int n = subArray[0].length; //width
         int maxLengthOfPath = m + n - 1;
-        printMatrix(subArray, m, n, 0, 0, new int[maxLengthOfPath], 0);
-        System.out.println();
+        constructMatrix(subArray, m, n, 0, 0, new int[maxLengthOfPath], 0);
 
         List<Integer> yee = new ArrayList<Integer>();
         for (int[] l : ls) {
@@ -157,24 +193,38 @@ public class MyProject {
         return findMin(yee);
     }
 
-
+    // We are using a Global variable due to the nature
+    // of the function and the inability to operate an efficient
+    // callback
     public static List<int[]> ls = new ArrayList<>();
-    private static void printMatrix(int mat[][], int m, int n,
-                                    int i, int j, int path[], int idx)
-    {
-        path[idx] = mat[i][j];
+
+
+    /**
+     * This function takes the input matrix (in this case top left to bottom right)
+     * of any path pattern, and construct every path between the two points.
+     * @param array
+     * @param m
+     * @param n
+     * @param i
+     * @param j
+     * @param path
+     * @param idx
+     */
+    private static void constructMatrix(int array[][], int m, int n,
+                                    int i, int j, int path[], int idx) {
+        path[idx] = array[i][j];
         // Reached the bottom of the matrix so we are left with
         // only option to move right
-        if (i == m - 1)
-        {
-            for (int k = j + 1; k < n; k++)
-            {
-                path[idx + k - j] = mat[i][k];
+        if (i == m - 1) {
+
+            // we must use a manual array copy
+            // due to the nature of the slave and master arrays
+            for (int k = j + 1; k < n; k++) {
+                path[idx + k - j] = array[i][k];
             }
 
-            int[] temp = new int[idx+n-j+1];
-            for (int l = 0; l < idx + n - j; l++)
-            {
+            int[] temp = new int[idx + n - j + 1];
+            for (int l = 0; l < idx + n - j; l++) {
                 temp[l] = path[l];
             }
             ls.add(temp);
@@ -183,63 +233,76 @@ public class MyProject {
 
         // Reached the right corner of the matrix we are left with
         // only the downward movement.
-
-        if (j == n - 1)
-        {
-            for (int k = i + 1; k < m; k++)
-            {
-                path[idx + k - i] = mat[k][j];
+        if (j == n - 1) {
+            for (int k = i + 1; k < m; k++) {
+                path[idx + k - i] = array[k][j];
 
             }
-            int[] temp = new int[idx+n-j+1];
+            int[] temp = new int[idx + n - j + 1];
 
-            for (int l = 0; l < idx + m - i; l++)
-            {
+            for (int l = 0; l < idx + m - i; l++) {
                 temp[l] = path[l];
             }
             ls.add(temp);
             return;
         }
 
-        // Print all the paths that are possible after moving down
-        printMatrix(mat, m, n, i + 1, j, path, idx + 1);
-
-        // Print all the paths that are possible after moving right
-        printMatrix(mat, m, n, i, j + 1, path, idx + 1);
+        //search paths moving right and down
+        constructMatrix(array, m, n, i + 1, j, path, idx + 1);
+        constructMatrix(array, m, n, i, j + 1, path, idx + 1);
 
     }
 
-    public static Integer findMin(List<Integer> list)
-    {
-        // check list is empty or not
+    /**
+     * This is a simple function which finds the Minimum
+     * value from an Integer List<>
+     * @param list input integer list
+     * @return the minimum value
+     */
+    public static Integer findMin(List<Integer> list) {
+        // check list is empty or not, and return
+        // the placeholder MAX_VALUE if necessary.
+        // This is mainly for test environment execution,
+        // but it allows the function to return something to work with.
         if (list == null || list.size() == 0) {
             return Integer.MAX_VALUE;
         }
-
         // create a new list to avoid modification
         // in the original list
         List<Integer> sortedlist = new ArrayList<>(list);
-
         // sort list in natural order
         Collections.sort(sortedlist);
-
         // first element in the sorted list
         // would be minimum
         return sortedlist.get(0);
     }
 
-    public int[] brightestPixelsInRowSegments(int[][] image, int[][] queries){
+    /**
+     * Compute the results of a list of queries on the given image.
+     * Each query will be a three-element int array {r, l, u} defining a row segment. You may assume l < u.
+     * A row segment is a set of pixels (r, c) such that r is as defined, l <= c, and c < u.
+     * For each query, find the value of the brightest pixel in the specified row segment.
+     * Return the query results in the same order as the queries are given.
+     * @param image image to pass to function
+     * @param queries the query to use to get the segment of the row
+     * @return array with the brightest pixels from query
+     */
+    public int[] brightestPixelsInRowSegments(int[][] image, int[][] queries) {
         int[] returns = new int[queries.length];
+        // Initialise index in order to iterate through each query and
+        // add it to the return array in the appropriate position.
         int ix = 0;
         for (int[] query : queries) {
-            int r = query[0]; int l = query[1]; int u = query[2];
+            int r = query[0];
+            int l = query[1];
+            int u = query[2];
             List<Integer> ls = new ArrayList<Integer>();
-            for(int i = l; i < u; i++){
+            for (int i = l; i < u; i++) {
                 ls.add(image[r][i]);
             }
             List<Integer> sorted = new ArrayList<>(ls);
             Collections.sort(sorted);
-            returns[ix] = sorted.get(sorted.size()-1);
+            returns[ix] = sorted.get(sorted.size() - 1);
             ix++;
         }
         return returns;
